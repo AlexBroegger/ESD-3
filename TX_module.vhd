267522758ed6886ba81c1a8cfca934ae328d8141
Alex B <alexreinholdbroegger@gmail.com>
2025-11-13 12:15:16 +0100

    Comments og syntaks

--- TX_module.vhd
+++ TX_module.vhd
@@ -9,8 +9,8 @@ entity TX is
 	reset : in std_logic;
 	data_available : in std_logic;
 	tx_line : out std_logic;
-	tx_data : in std_logic_vector(7 downto 0);
-	tx_busy : in std_logic
+	tx_data : in unsigned(7 downto 0);
+	tx_busy : out std_logic
 	);
 	
 end TX;
@@ -18,18 +18,18 @@ end TX;
 architecture rtl of TX is
 
 	-- Defintion af States
-	TYPE Tstate IS (IDLE, START, DATA, STOP);
+	TYPE Tstate IS (IDLE, START, DATA, STOP); -- States: Intet parity, 1 stop bit og 8 data bits.
 	SIGNAL state: Tstate;
 	SIGNAL next_state: Tstate;
 	
 	-- Signaler til at shifte bits ud til tx_linje, og tælle dem
-	SIGNAL shift_reg: unsigned(7 downto 0);
-	SIGNAL bit_count: integer range 0 to 7;
+	SIGNAL shift_reg: unsigned(7 downto 0); 
+	SIGNAL bit_count: integer range 0 to 7; -- Counter til state transition
 	
 	-- Klok signaler
 	constant clock_p_bit : integer := 1250; --12000000/9600 = 12MHz/9600 baud rate = 1250
-	signal baud_count : integer range 0 to clock_p_bit-1;
-	signal baud_tick : std_logic;
+	signal baud_count : integer range 0 to clock_p_bit-1: -- Baud counter, som bruges til at tælle op til 1249
+	signal baud_tick : std_logic; -- Når baud counter kommer op til 1250, bliver baud_tick slået til logisk 1.
 	
 begin
 
@@ -40,10 +40,10 @@ begin
 			baud_count <= 0;
 			baud_tick <= '0';
 
-		elsif rising_edge(clk) then
+		elsif rising_edge(clk) then -- hvert rising edge
 			baud_tick <= '0';
 			
-			if baud_count = clock_p_bit; then
+			if baud_count = clock_p_bit-1 then
 				baud_count <= 0;
 				baud_tick <= '1';
 			else
@@ -58,29 +58,32 @@ begin
 	
 		IF reset = '1' then
 			state <= IDLE;
-			shift_reg <= (others => 0);
+			shift_reg <= (others => '0');
 			bit_count <= 0;
-			tx_line <= '1';
 			
 		ELSIF rising_edge(clk) then
 			state <= next_state;
 			CASE state IS
+
 				WHEN IDLE =>
 					if data_available = '1' then
-						shift_reg <= tx_data;
+						shift_reg <= unsigned(tx_data);
 						bit_count <= 0;
 						baud_count <= 0;
 					end if;
+
 				WHEN START =>
 					if baud_tick = '1' then
 						baud_count <= 0;
 					end if;
+
 				WHEN DATA =>
 					if baud_tick = '1' then
-						shift_reg <= 0 & shift_reg(7 downto 1);
-						bit_count <= bit_count+1
+						shift_reg <= '0' & shift_reg(7 downto 1); -- Right Bit shifter. Sammenkæder et 0 på data bits.
+						bit_count <= bit_count+1;
 						baud_count <= 0;
 					end if;
+
 				when STOP =>
 					if baud_tick = '1' THEN
 						baud_count <= 0;
@@ -92,34 +95,34 @@ begin
 	
 	next_state_proc : process (state, data_available, bit_count, baud_tick) -- Logik til state transition
 	begin
-		-- Insert logic HERE
+	
 		case state IS
 		
-			WHEN IDLE =>
+			WHEN IDLE => -- IDLE state transition
 				if data_available = '1' then
 					next_state <= START;
 				else
 					next_state <= IDLE;
 				end if;
 				
-			WHEN START =>
+			WHEN START => -- START state transition
 				if baud_tick = '1' then
 					next_state <= DATA;
-				elsif
+                else
 					next_state <= START;
 				end if;
 				
-			WHEN DATA =>
+			WHEN DATA => -- DATA state transition
 				if bit_count = 7 and baud_tick='1' then
 					next_state <= STOP;
 				else
 					next_state <= DATA;
 				end if;
 				
-			WHEN STOP => 
+			WHEN STOP => -- STOP state transition
 				if baud_tick = '1' THEN
 					next_state <= IDLE;
-				elsif
+                else
 					next_state <= STOP;
 				end if;
 				
@@ -132,7 +135,7 @@ begin
 		case state IS
 			WHEN IDLE =>
 				tx_line <= '1';
-				tx_busy <= '0';
+				tx_busy <= '0'; -- Ikke busy fordi der ikke skal ske noget på tx_linje
 			WHEN START =>
 				tx_line <= '0';
 				tx_busy <= '1';
@@ -143,5 +146,7 @@ begin
 				tx_line <= '1';
 				tx_busy <= '1';
 		end case;
+
 	end process state_output;
+
 end rtl;
