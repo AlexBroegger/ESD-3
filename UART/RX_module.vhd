4d2e359b4656ae5c0b1bc711a5ab7b48ef1ec58d
Alex <id87hs@student.aau.dk>
2025-12-11 22:18:04 +0100

    Fixed timing error

--- UART/RX_module.vhd
+++ UART/RX_module.vhd
@@ -4,14 +4,14 @@ use ieee.numeric_std.all;
 
 entity RX is
     port (
-        clk        : in  std_logic; -- Giver sig selv
-        reset      : in  std_logic; -- Reset knap, den skal bare være der
-        rx_line    : in  std_logic; -- Incoming fra TX line
-        rx_data    : out STD_LOGIC_VECTOR(7 downto 0); -- Bliver samplet med shift register
-        rx_ready   : out std_logic; -- Bare om han er klar
+        clk        : in  std_logic;
+        reset      : in  std_logic;
+        rx_line    : in  std_logic;
+        rx_data    : out STD_LOGIC_VECTOR(7 downto 0);
+        rx_ready   : out std_logic;
         parity_enable : in std_logic; -- parity_enable = 0 er slukket for parity, og tændte for = 1
         parity_m : in std_logic; -- parity_m = 0 er even parity, parity_m = 1 er odd parity
-        parity_valid : out std_logic -- parity_valid = 0, fanget parity bit er falsk, ellers er det sandt
+        parity_valid : out std_logic
     );
 end RX;
 
@@ -80,11 +80,15 @@ begin
             os_tick  <= '0';
         elsif rising_edge(clk) then
             os_tick <= '0';
-            if os_count = 77 then
+            if state = idle then
                 os_count <= 0;
-                os_tick  <= '1';
             else
-                os_count <= os_count + 1;
+                if os_count = 77 then
+                    os_count <= 0;
+                    os_tick  <= '1';
+                else
+                    os_count <= os_count + 1;
+                end if;
             end if;
         end if;
     end process;
@@ -109,7 +113,7 @@ begin
                 bit_count    <= 0; -- Reset
             
             elsif os_tick='1' then
-                -- Standard counter
+                -- Standard counter that wraps around
                 if sample_count = 15 then
                     sample_count <= 0;
                     -- Ingen tick fordi det ikke skal bruges, vi skal bare tælle ticks
@@ -119,8 +123,10 @@ begin
 
                 -- Data shift
                 if shift_en='1' then
+                    
                     shift_reg <= rx_sync_2 & shift_reg(7 downto 1); -- Concatetion (tror det hedder det)
                     bit_count <= bit_count + 1; -- bit_count styring
+                    
                 end if;
                 if sample_parity = '1' then 
                     paritys <= rx_sync_2; 
@@ -196,7 +202,6 @@ begin
             when IDLE =>
             -- Intet
             when START =>
-            -- Intet
             when DATA =>
                 if os_tick='1' and sample_count = 8 then
                     shift_en <= '1';
